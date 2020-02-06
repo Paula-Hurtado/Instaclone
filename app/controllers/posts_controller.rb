02e3b44a79b0c115b89f12691594520b7cbfbf5c
Paula Hurtado💜 <paulahurtado@Paulas-MacBook-Air.local>
2020-02-06 14:28:37 -0500

    my second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,12 +16,47 @@ class PostsController < ApplicationController
             flash[:alert] = "post creation failed"
          end 
     end
+
     def show 
         @post = Post.find(params[:id])
     end 
 
+    def destroy
+        post = Post.find(params[:id])
+        if current_user == post.user
+            post.destroy
+            redirect_to "/posts"
+            flash[:notice] = "Post destroyed"
+        else 
+            redirect_back(fallback_location: root_path)
+            flash[:alert] = "Not authorized to delete post"
+        end 
+    end
+    def update
+        if current_user == post.user
+            post = Post.find(params[:id])
+            post.update(post_params)
+            redirect_to "/posts" 
+            flash[:notice] = "Update successful!"
+        else 
+            redirect_back(fallback_location: root_path)
+            flash[:alert] = "Not authorized to edit post"
+        end 
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+        if current_user != @post.user
+            redirect_back(fallback_location: root_path)
+            flash[:alert] = "Not authorized to edit post"
+        end  
+    end
+
     private
     def post_params 
         params.require(:post).permit(:caption, :pic)
+    end 
+    def update_params 
+        params.require(:post).permit(:caption)
     end 
 end
